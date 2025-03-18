2db34cb84cfb2caee1af0e00297ea4dea3021265
julianguyn <julianguyenpt@gmail.com>
2025-03-18 16:59:54 -0400

    feat: feature influence

--- feature_influence.R
+++ feature_influence.R
@@ -62,64 +62,68 @@ fcrc_k <- 0.1
 ############################################################
 
 # function to split stability dataframe for each dataset pair
-stability_pair <- function(stability) {
-  #todo
+stability_pair <- function(stability, pair, kuncheva) {
+
+  n <- nrows(stability)
+  
+  if (pair == "gcsi_ccle") {
+    stability <- stability[,c('gdsc_median','gc','n_exon','length','gcsi_ccle_spearman')]
+    stable <- stability[order(-stability$gcsi_ccle_spearman),]
+    unstable <- stability[order(stability$gcsi_ccle_spearman),]
+  }
+  if (pair == "gcsi_gdsc") {
+    stability <- stability[,c('ccle_median','gc','n_exon','length','gcsi_gdsc_spearman')]
+    stable <- stability[order(-stability$gcsi_gdsc_spearman),]
+    unstable <- stability[order(stability$gcsi_gdsc_spearman),]
+  }
+  if (pair == "gdsc_ccle") {
+    stability <- stability[,c('gcsi_median','gc','n_exon','length','gdsc_ccle_spearman')]
+    stable <- stability[order(-stability$gdsc_ccle_spearman),]
+    unstable <- stability[order(stability$gdsc_ccle_spearman),]
+  }
+
+  # subset based on kuncheva index
+  stable <- stable[1:(kuncheva*n),]
+  unstable <- unstable[1:(kuncheva*n),]
+  stability <- rbind(stable,unstable)
+
+  # shuffle data
+  stability <- stability[sample(1:nrow(stability)),]
+
+  return(stability)
 }
 
-#Make a copy for each dataset pair
-transcript_stability_gcsi_ccle <- transcript_stability
-transcript_stability_gdsc_ccle <- transcript_stability
-transcript_stability_gcsi_gdsc <- transcript_stability
+gene_stability_gcsi_ccle <- stability_pair(gene_stability, "gcsi_ccle", gene_k)
+gene_stability_gcsi_gdsc <- stability_pair(gene_stability, "gcsi_gdsc", gene_k)
+gene_stability_gdsc_ccle <- stability_pair(gene_stability, "gdsc_ccle", gene_k)
+
+transcript_stability_gcsi_ccle <- stability_pair(transcript_stability, "gcsi_ccle", isof_k)
+transcript_stability_gcsi_gdsc <- stability_pair(transcript_stability, "gcsi_gdsc", isof_k)
+transcript_stability_gdsc_ccle <- stability_pair(transcript_stability, "gdsc_ccle", isof_k)
+
+ciri_stability_gcsi_ccle <- stability_pair(ciri_stability, "gcsi_ccle", ciri_k)
+ciri_stability_gcsi_gdsc <- stability_pair(ciri_stability, "gcsi_gdsc", ciri_k)
+ciri_stability_gdsc_ccle <- stability_pair(ciri_stability, "gdsc_ccle", ciri_k)
+
+circ_stability_gcsi_ccle <- stability_pair(circ_stability, "gcsi_ccle", circ_k)
+circ_stability_gcsi_gdsc <- stability_pair(circ_stability, "gcsi_gdsc", circ_k)
+circ_stability_gdsc_ccle <- stability_pair(circ_stability, "gdsc_ccle", circ_k)
+
+cfnd_stability_gcsi_ccle <- stability_pair(cfnd_stability, "gcsi_ccle", cfnd_k)
+cfnd_stability_gcsi_gdsc <- stability_pair(cfnd_stability, "gcsi_gdsc", cfnd_k)
+cfnd_stability_gdsc_ccle <- stability_pair(cfnd_stability, "gdsc_ccle", cfnd_k)
+
+fcrc_stability_gcsi_ccle <- stability_pair(fcrc_stability, "gcsi_ccle", fcrc_k)
+fcrc_stability_gcsi_gdsc <- stability_pair(fcrc_stability, "gcsi_gdsc", fcrc_k)
+fcrc_stability_gdsc_ccle <- stability_pair(fcrc_stability, "gdsc_ccle", fcrc_k)
 
 
-#Isolate MOST stable/unstable transcripts (15% quantile)
-#gCSI/CCLE
-stable <- transcript_stability_gcsi_ccle[order(-transcript_stability_gcsi_ccle$gcsi_ccle_spearman),]
-stable <- stable[1:9037,] #top 15%
-unstable <- transcript_stability_gcsi_ccle[order(transcript_stability_gcsi_ccle$gcsi_ccle_spearman),]
-unstable <- unstable[1:9037,]
-transcript_stability_gcsi_ccle <- rbind(stable,unstable)
 
-#gCSI/GDSC
-stable <- transcript_stability_gcsi_gdsc[order(-transcript_stability_gcsi_gdsc$gcsi_gdsc_spearman),]
-stable <- stable[1:9037,]
-unstable <- transcript_stability_gcsi_gdsc[order(transcript_stability_gcsi_gdsc$gcsi_gdsc_spearman),]
-unstable <- unstable[1:9037,]
-transcript_stability_gcsi_gdsc <- rbind(stable,unstable)
+############################################################
+# Permutation feature importance
+############################################################
 
-#GDSC/CCLE
-stable <- transcript_stability_gdsc_ccle[order(-transcript_stability_gdsc_ccle$gdsc_ccle_spearman),]
-stable <- stable[1:9037,]
-unstable <- transcript_stability_gdsc_ccle[order(transcript_stability_gdsc_ccle$gdsc_ccle_spearman),]
-unstable <- unstable[1:9037,]
-transcript_stability_gdsc_ccle <- rbind(stable,unstable)
-
-
-#Extract features + stability from df
-transcript_stability_gcsi_ccle <- transcript_stability_gcsi_ccle[,c('gdsc_median','gc','n_exon','single_read_mappability','multi_read_mappability','length','three_prime_bias','five_prime_bias','gcsi_ccle_spearman')]
-transcript_stability_gcsi_gdsc <- transcript_stability_gcsi_gdsc[,c('ccle_median','gc','n_exon','single_read_mappability','multi_read_mappability','length','three_prime_bias','five_prime_bias','gcsi_gdsc_spearman')]
-transcript_stability_gdsc_ccle <- transcript_stability_gdsc_ccle[,c('gcsi_median','gc','n_exon','single_read_mappability','multi_read_mappability','length','three_prime_bias','five_prime_bias','gdsc_ccle_spearman')]
-
-#shuffle data
-transcript_stability_gcsi_ccle <- transcript_stability_gcsi_ccle[sample(1:nrow(transcript_stability_gcsi_ccle)),]
-transcript_stability_gcsi_gdsc <- transcript_stability_gcsi_gdsc[sample(1:nrow(transcript_stability_gcsi_gdsc)),]
-transcript_stability_gdsc_ccle <- transcript_stability_gdsc_ccle[sample(1:nrow(transcript_stability_gdsc_ccle)),]
-
-
-########################################################
-########### Permutation Feature Importance #############
-########################################################
-
-
-################# 
-# Reduce size of data.frame for Code Ocean, comment out following 3 lines for full result
-transcript_stability_gcsi_ccle <- transcript_stability_gcsi_ccle[1:500,]
-transcript_stability_gcsi_gdsc <- transcript_stability_gcsi_gdsc[1:500,]
-transcript_stability_gdsc_ccle <- transcript_stability_gdsc_ccle[1:500,]
-#################
-
-
-## Run Linear Regression
+# function to run Linear Regression
 runLinearReg <- function(x, label) {
   
   # Create dataframe to store feature scores
@@ -132,8 +136,7 @@ runLinearReg <- function(x, label) {
   #10-fold CV
   fitControl <- trainControl(method = "repeatedcv",
                              number = 10,
-                             repeats = 5) #change to 10 for full results
-  
+                             repeats = 10) 
   
   # Loop through each feature
   for (i in 1:length(colnames(x)[1:8])) {
@@ -149,7 +152,7 @@ runLinearReg <- function(x, label) {
     baseline_score <- (gbmFit$results$RMSE)^2
     
     # Permute each feature 20,000 times
-    for (k in 1:2) { # change to 20,000 for full results
+    for (k in 1:20000) { 
       
       # shuffle feature column
       shuffle_feat <- sample(x[,i])
@@ -171,60 +174,102 @@ runLinearReg <- function(x, label) {
 }
 
 
-lm_gcsi_ccle <- runLinearReg(transcript_stability_gcsi_ccle, "gcsi_ccle")
-lm_gcsi_gdsc <- runLinearReg(transcript_stability_gcsi_gdsc, "gcsi_gdsc")
-lm_gdsc_ccle <- runLinearReg(transcript_stability_gdsc_ccle, "gdsc_ccle")
+gene_stability_gcsi_ccle <- runLinearReg(gene_stability_gcsi_ccle, "gcsi_ccle")
+gene_stability_gcsi_gdsc <- runLinearReg(gene_stability_gcsi_gdsc, "gcsi_gdsc")
+gene_stability_gdsc_ccle <- runLinearReg(gene_stability_gdsc_ccle, "gdsc_ccle")
 
-lm_feature_results <- rbind(lm_gcsi_ccle, lm_gcsi_gdsc, lm_gdsc_ccle)
+transcript_stability_gcsi_ccle <- runLinearReg(transcript_stability_gcsi_ccle, "gcsi_ccle")
+transcript_stability_gcsi_gdsc <- runLinearReg(transcript_stability_gcsi_gdsc, "gcsi_gdsc")
+transcript_stability_gdsc_ccle <- runLinearReg(transcript_stability_gdsc_ccle, "gdsc_ccle")
+
+ciri_stability_gcsi_ccle <- runLinearReg(ciri_stability_gcsi_ccle, "gcsi_ccle")
+ciri_stability_gcsi_gdsc <- runLinearReg(ciri_stability_gcsi_gdsc, "gcsi_gdsc")
+ciri_stability_gdsc_ccle <- runLinearReg(ciri_stability_gdsc_ccle, "gdsc_ccle")
+
+circ_stability_gcsi_ccle <- runLinearReg(circ_stability_gcsi_ccle, "gcsi_ccle")
+circ_stability_gcsi_gdsc <- runLinearReg(circ_stability_gcsi_gdsc, "gcsi_gdsc")
+circ_stability_gdsc_ccle <- runLinearReg(circ_stability_gdsc_ccle, "gdsc_ccle")
+
+cfnd_stability_gcsi_ccle <- runLinearReg(cfnd_stability_gcsi_ccle, "gcsi_ccle")
+cfnd_stability_gcsi_gdsc <- runLinearReg(cfnd_stability_gcsi_gdsc, "gcsi_gdsc")
+cfnd_stability_gdsc_ccle <- runLinearReg(cfnd_stability_gdsc_ccle, "gdsc_ccle")
+
+fcrc_stability_gcsi_ccle <- runLinearReg(fcrc_stability_gcsi_ccle, "gcsi_ccle")
+fcrc_stability_gcsi_gdsc <- runLinearReg(fcrc_stability_gcsi_gdsc, "gcsi_gdsc")
+fcrc_stability_gdsc_ccle <- runLinearReg(fcrc_stability_gdsc_ccle, "gdsc_ccle")
+
+############################################################
+# Combine results for each transcript type
+############################################################
+
+gene_features <- rbind(gene_stability_gcsi_ccle, gene_stability_gcsi_gdsc, gene_stability_ggdsc_ccle)
+transcript_features <- rbind(transcript_stability_gcsi_ccle, transcript_stability_gcsi_gdsc, transcript_stability_ggdsc_ccle)
+ciri_features <- rbind(ciri_stability_gcsi_ccle, ciri_stability_gcsi_gdsc, ciri_stability_ggdsc_ccle)
+circ_features <- rbind(circ_stability_gcsi_ccle, circ_stability_gcsi_gdsc, circ_stability_ggdsc_ccle)
+cfnd_features <- rbind(cfnd_stability_gcsi_ccle, cfnd_stability_gcsi_gdsc, cfnd_stability_ggdsc_ccle)
+fcrc_features <- rbind(fcrc_stability_gcsi_ccle, fcrc_stability_gcsi_gdsc, fcrc_stability_ggdsc_ccle)
 
 
+############################################################
+# Format results for plotting
+############################################################
 
-###############
-#### PLOTS ####
-###############
+# function to format results for plotting
+format_features <- function(features) {
+  #rename datasets
+  features <- features %>% 
+    mutate(dataset = recode(dataset, "gcsi_ccle" = "gCSI/CCLE", "gcsi_gdsc" = "gCSI/GDSC", "gdsc_ccle" = "GDSC/CCLE"))
 
-load("C:/Users/julia/Desktop/BHK Lab/permutations/h4h/h4h_lm_results.RData")
+  #rename features
+  features <- features %>% 
+    mutate(feature = recode(feature, "median" = "Median Expression", "gc" = "GC%", 
+                            "n_exon" = "Number of Exons", "length" = "Length"))
+  features$factor <- factor(features$feature, 
+                     levels=c('Median Expression', 'GC%', 'Number of Exons', 'Length'))
 
-#rename datasets
-lm_feature_results <- lm_feature_results %>% 
-  mutate(dataset = recode(dataset, "gcsi_ccle" = "gCSI/CCLE", "gcsi_gdsc" = "gCSI/GDSC", "gdsc_ccle" = "GDSC/CCLE"))
+  # compute difference
+  features$difference <- with(features, abs(baseline - permuted))
 
-#rename features
-lm_feature_results <- lm_feature_results %>% 
-  mutate(feature = recode(feature, "median" = "Median Expression", "gc" = "GC%", "n_exon" = "Number of Exons", "length" = "Length",
-                          "single_read_mappability" = "Single Read Mappability", "multi_read_mappability" = "Multi Read Mappability",
-                          "three_prime_bias" = "Three Prime Bias", "five_prime_bias" = "Five Prime Bias"))
-
-
-lm_feature_results$factor <- factor(lm_feature_results$feature, 
-                                    levels=c('Median Expression', 'GC%', 'Number of Exons', 'Length', 'Single Read Mappability', 'Multi Read Mappability', 'Three Prime Bias', 'Five Prime Bias'))
-
-
-#### Feature Influence ####
-
-lm_feature_results$difference <- with(lm_feature_results, abs(baseline - permuted))
-
-# function from http://www.sthda.com/english/wiki/ggplot2-error-bars-quick-start-guide-r-software-and-data-visualization
-data_summary <- function(data, varname, groupnames){
-  require(plyr)
-  summary_func <- function(x, col){
-    c(mean = mean(x[[col]], na.rm=TRUE),
-      sd = sd(x[[col]], na.rm=TRUE))
+  # function from http://www.sthda.com/english/wiki/ggplot2-error-bars-quick-start-guide-r-software-and-data-visualization
+  data_summary <- function(data, varname, groupnames){
+    require(plyr)
+    summary_func <- function(x, col){
+      c(mean = mean(x[[col]], na.rm=TRUE),
+        sd = sd(x[[col]], na.rm=TRUE))
+    }
+    data_sum<-ddply(data, groupnames, .fun=summary_func,
+                    varname)
+    data_sum <- rename(data_sum, c("mean" = varname))
+    return(data_sum)
   }
-  data_sum<-ddply(data, groupnames, .fun=summary_func,
-                  varname)
-  data_sum <- rename(data_sum, c("mean" = varname))
-  return(data_sum)
-}
-lm_res <- data_summary(lm_feature_results, varname = "difference", groupnames = c("factor", "dataset"))
 
-p1 <- ggplot(data = lm_res, aes(x=factor, y=difference, fill = dataset)) + 
+  lm_res <- data_summary(features, varname = "difference", groupnames = c("factor", "dataset"))
+
+  return(lm_res)
+}
+
+gene_res <- format_features(gene_features)
+transcript_res <- format_features(transcript_features)
+ciri_res <- format_features(ciri_features)
+circ_res <- format_features(circ_features)
+cfnd_res <- format_features(cfnd_features)
+fcrc_res <- format_features(fcrc_features)
+
+
+
+############################################################
+# Plot feature influence
+############################################################
+
+plot_features <- function(res) {
+
+  p <- ggplot(data = res, aes(x=factor, y=difference, fill = dataset)) + 
   geom_bar(stat="identity", color="black", position=position_dodge()) +
   geom_errorbar(aes(ymin=difference-sd, ymax=difference+sd), width=.2, position=position_dodge(.9)) +
   ylab(expression('MSE - MSE'['permuted'])) + xlab("\nFeature") + 
   scale_fill_manual(guide = guide_legend(reverse = FALSE, title = "Dataset"), labels=c("gCSI/CCLE","gCSI/GDSC","GDSC/CCLE"), values = c("#392C57", "#51C7AD", "#3670A0")) + 
-  scale_x_discrete(limits = c('Median Expression', 'GC%', 'Number of Exons', 'Length', 'Single Read Mappability', 'Multi Read Mappability', 'Three Prime Bias', 'Five Prime Bias'),
-                   labels=c('Median\nExpression', 'GC%', 'Number\nof Exons', 'Length', 'Single Read\nMappability','Multi Read\nMappability', 'Three Prime\nBias','Five Prime\nBias')) + 
+  scale_x_discrete(limits = c('Median Expression', 'GC%', 'Number of Exons', 'Length'),
+                   labels=c('Median\nExpression', 'GC%', 'Number\nof Exons', 'Length')) + 
   theme_classic() + scale_y_continuous(limits = c(0, 0.032), expand=c(0,0)) +
   theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5),
         text = element_text(size = 15), 
@@ -232,8 +277,32 @@ p1 <- ggplot(data = lm_res, aes(x=factor, y=difference, fill = dataset)) +
         axis.text.x = element_text(size=12, vjust = 0.5), 
         plot.title = element_text(hjust = 0.5, size = 17), 
         legend.position = "bottom") + 
-  guides(guide_legend(title.vjust = 2.8)) 
+  guides(guide_legend(title.vjust = 2.8))
 
-png("../results/figure3.png", width=275, height=150, units='mm', res = 600, pointsize=80)
-p1
+  return(p)
+}
+ 
+
+png("../results/figures/figure8/gene_features.png", width=275, height=150, units='mm', res = 600, pointsize=80)
+plot_features(gene_res)
+dev.off()
+
+png("../results/figures/figure8/transcript_features.png", width=275, height=150, units='mm', res = 600, pointsize=80)
+plot_features(transcript_res)
+dev.off()
+
+png("../results/figures/figure8/ciri_features.png", width=275, height=150, units='mm', res = 600, pointsize=80)
+plot_features(ciri_res)
+dev.off()
+
+png("../results/figures/figure8/circ_features.png", width=275, height=150, units='mm', res = 600, pointsize=80)
+plot_features(circ_res)
+dev.off()
+
+png("../results/figures/figure8/cfnd_features.png", width=275, height=150, units='mm', res = 600, pointsize=80)
+plot_features(cfnd_res)
+dev.off()
+
+png("../results/figures/figure8/fcrc_features.png", width=275, height=150, units='mm', res = 600, pointsize=80)
+plot_features(fcrc_res)
 dev.off()
