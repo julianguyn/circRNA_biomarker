c5fd1cb8e533c0949e81c72c78568dcb2020615b
julianguyn <julianguyenpt@gmail.com>
2025-08-07 15:20:16 -0400

    feat: modify MYC mapping

--- biomarker_myc.R
+++ biomarker_myc.R
@@ -2,35 +2,116 @@
 suppressPackageStartupMessages({
     library(data.table)
     library(ggplot2)
-    library(ComplexHeatmap)
-    library(ggh4x)
-    library(ggVennDiagram)
-    library(stringr)
+    library(ggrepel)
 })
 
 
 ############################################################
-# Load in circRNA gene data
+# Load in circRNA expression data
 ############################################################
 
+path = "../data/processed_cellline/all_samples/"
+
+# load circRNA expression data
+ciri_gcsi <- fread(paste0(path, "CIRI2/ciri_gcsi_counts.tsv"), data.table = F)
+ciri_gdsc <- fread(paste0(path, "CIRI2/ciri_gdsc_counts.tsv"), data.table = F)
+ciri_ccle <- fread(paste0(path, "CIRI2/ciri_ccle_counts.tsv"), data.table = F)
+
+circ_gcsi <- fread(paste0(path, "CIRCexplorer2/circ_gcsi_counts.tsv"), data.table = F)
+circ_gdsc <- fread(paste0(path, "CIRCexplorer2/circ_gdsc_counts.tsv"), data.table = F)
+circ_ccle <- fread(paste0(path, "CIRCexplorer2/circ_ccle_counts.tsv"), data.table = F)
+
+cfnd_gcsi <- fread(paste0(path, "circRNA_finder/cfnd_gcsi_counts.tsv"), data.table = F)
+cfnd_gdsc <- fread(paste0(path, "circRNA_finder/cfnd_gdsc_counts.tsv"), data.table = F)
+cfnd_ccle <- fread(paste0(path, "circRNA_finder/cfnd_ccle_counts.tsv"), data.table = F)
+
+fcrc_gcsi <- fread(paste0(path, "find_circ/fcrc_gcsi_counts.tsv"), data.table = F)
+fcrc_gdsc <- fread(paste0(path, "find_circ/fcrc_gdsc_counts.tsv"), data.table = F)
+fcrc_ccle <- fread(paste0(path, "find_circ/fcrc_ccle_counts.tsv"), data.table = F)
+
+
+############################################################
+# Identify circRNA mapping to MYC
+############################################################
+
+# save all circRNAs on chr8
+all_circ <- c(colnames(ciri_gcsi), colnames(ciri_gdsc), colnames(ciri_ccle),
+              colnames(circ_gcsi), colnames(circ_gdsc), colnames(circ_ccle),
+              colnames(cfnd_gcsi), colnames(cfnd_gdsc), colnames(cfnd_ccle),
+              colnames(fcrc_gcsi), colnames(fcrc_gdsc), colnames(fcrc_ccle))
+chr8 <- gsub("chr8\\.", "", all_circ[grep("chr8", all_circ)])
+split_vals <- strsplit(chr8, "\\.")
+df <- data.frame(
+    circ = paste0("chr8.", chr8),
+    start = as.numeric(sapply(split_vals, `[`, 1)),
+    end = as.numeric(sapply(split_vals, `[`, 2)),
+    myc = 0
+)
+
+# get MYC bases (MYC gene: 8:127735434-127742951)
+myc_start = 127735434
+myc_end = 127742951
+myc <- myc_start:myc_end
+
+# save number of overlapping bases per circRNA on chr8
+for (i in seq_along(df$circ)) {
+    circ <- df$start[i]:df$end[i]
+    if (sum(circ %in% myc) > 0) {
+        df$myc[i] <- sum(circ %in% myc)
+    }
+}
+
+# identify circRNAs that overlapp 100% with the MYC gene 
+df$total <- df$end - df$start + 1
+df$prop <- df$myc / df$total * 100
+
+# plot proportion distribution
+png("../results/figures/figure9/myc/circ_proportion_overlap.png", width = 7.5, height = 6, res = 600, units = "in")
+ggplot(df, aes(x = prop)) + 
+    geom_histogram(color="black", fill="#C57B57") + 
+    geom_text(stat = "bin", aes(label = after_stat(count)), vjust = -1) +
+    theme_classic() + 
+    labs(x = "Proportion Overlap of CircRNA Transcript and MYC", y = "Count")
+dev.off()
+
+############################################################
+# Keep circRNA overlapping 100% to MYC
+############################################################
+
+df <- df[df$prop == 100,] #n=508
+
+# define function to filter circ exp dataframes
+filter_myc <- function(circ) {
+
+    rownames(circ) <- circ$sample
+    circ <- circ[,colnames(circ) %in% df$circ]
+    print(ncol(circ))
+
+    return(circ)
+}
+
+ciri_gcsi <- filter_myc(ciri_gcsi) #8
+ciri_gdsc <- filter_myc(ciri_gdsc) #2
+ciri_ccle <- filter_myc(ciri_ccle) #9
+
+circ_gcsi <- filter_myc(circ_gcsi) #85
+circ_gdsc <- filter_myc(circ_gdsc) #3
+circ_ccle <- filter_myc(circ_ccle) #19
+
+cfnd_gcsi <- filter_myc(cfnd_gcsi) #116
+cfnd_gdsc <- filter_myc(cfnd_gdsc) #3
+cfnd_ccle <- filter_myc(cfnd_ccle) #155
+
+fcrc_gcsi <- filter_myc(fcrc_gcsi) #41
+fcrc_gdsc <- filter_myc(fcrc_gdsc) #11
+fcrc_ccle <- filter_myc(fcrc_ccle) #56
+
 # save dataframes
-path = "../data/processed_cellline/GE_all_samples/"
-
-ciri_gcsi_ge <- fread(paste0(path, "CIRI2/ciri_gcsi_counts.tsv"), data.table = F)
-ciri_gdsc_ge <- fread(paste0(path, "CIRI2/ciri_gdsc_counts.tsv"), data.table = F)
-ciri_ccle_ge <- fread(paste0(path, "CIRI2/ciri_ccle_counts.tsv"), data.table = F)
-
-circ_gcsi_ge <- fread(paste0(path, "CIRCexplorer2/circ_gcsi_counts.tsv"), data.table = F)
-circ_gdsc_ge <- fread(paste0(path, "CIRCexplorer2/circ_gdsc_counts.tsv"), data.table = F)
-circ_ccle_ge <- fread(paste0(path, "CIRCexplorer2/circ_ccle_counts.tsv"), data.table = F)
-
-cfnd_gcsi_ge <- fread(paste0(path, "circRNA_finder/cfnd_gcsi_counts.tsv"), data.table = F)
-cfnd_gdsc_ge <- fread(paste0(path, "circRNA_finder/cfnd_gdsc_counts.tsv"), data.table = F)
-cfnd_ccle_ge <- fread(paste0(path, "circRNA_finder/cfnd_ccle_counts.tsv"), data.table = F)
-
-fcrc_gcsi_ge <- fread(paste0(path, "find_circ/fcrc_gcsi_counts.tsv"), data.table = F)
-fcrc_gdsc_ge <- fread(paste0(path, "find_circ/fcrc_gdsc_counts.tsv"), data.table = F)
-fcrc_ccle_ge <- fread(paste0(path, "find_circ/fcrc_ccle_counts.tsv"), data.table = F)
+save(ciri_gcsi, ciri_gdsc, ciri_ccle,
+     circ_gcsi, circ_gdsc, circ_ccle,
+     cfnd_gcsi, cfnd_gdsc, cfnd_ccle,
+     fcrc_gcsi, fcrc_gdsc, fcrc_ccle,
+     file = "../results/data/circ_myc.RData")
 
 
 ############################################################
@@ -45,159 +126,159 @@ belinostat <- ctrp_sen[rownames(ctrp_sen) == "Belinostat",,drop=F]
 belinostat <- belinostat[,colSums(is.na(belinostat))<nrow(belinostat)]
 
 
-############################################################
-# Keep only circRNAs mapped to MYC gene
-############################################################
-
-keep_myc <- function(counts) {
-    if ("MYC" %in% colnames(counts)) {
-        counts <- counts[,colnames(counts) %in% c("sample", "MYC")]
-        counts[counts == 0] <- NA
-        rownames(counts) <- counts$sample
-        counts$sample <- NULL
-        return(counts)
-    }
-    return(0)
-}
-
-ciri_gcsi_ge <- keep_myc(ciri_gcsi_ge)
-ciri_gdsc_ge <- keep_myc(ciri_gdsc_ge)
-ciri_ccle_ge <- keep_myc(ciri_ccle_ge)
-
-circ_gcsi_ge <- keep_myc(circ_gcsi_ge)
-circ_gdsc_ge <- keep_myc(circ_gdsc_ge)
-circ_ccle_ge <- keep_myc(circ_ccle_ge)
-
-cfnd_gcsi_ge <- keep_myc(cfnd_gcsi_ge)
-cfnd_gdsc_ge <- keep_myc(cfnd_gdsc_ge)
-cfnd_ccle_ge <- keep_myc(cfnd_ccle_ge)
-
-fcrc_gcsi_ge <- keep_myc(fcrc_gcsi_ge)
-fcrc_gdsc_ge <- keep_myc(fcrc_gdsc_ge)
-fcrc_ccle_ge <- keep_myc(fcrc_ccle_ge)
 
 
 ############################################################
-# Check number of 0 expression
-############################################################
-
-nrow(ciri_ccle_ge[ciri_ccle_ge$MYC == 0,]) #504/524
-nrow(circ_ccle_ge[circ_ccle_ge$MYC == 0,]) #519/524
-nrow(cfnd_ccle_ge[cfnd_ccle_ge$MYC == 0,]) #489/524
-nrow(fcrc_ccle_ge[fcrc_ccle_ge$MYC == 0,]) #69/524
-
-
-############################################################
-# Binarize transcript expression by median
+# Binarize transcript expression by zero expression
 ############################################################
 
 # function to compute drug response associations
-binary_dr <- function(counts_df, drug_df, pset) {
+binary_dr <- function(counts_df, drug_df, label) {
+
+    features <- colnames(counts_df)
 
     # create data frame to hold results
-    combinations <- expand.grid(Drug = rownames(drug_df), Feature = colnames(counts_df))
-    combinations$num_samples <- combinations$pval <-  combinations$W <- NA
-
-    # compute median
-    median <- median(counts_df$MYC, na.rm = T)
+    combinations <- expand.grid(Drug = rownames(drug_df), Feature = features)
+    combinations$num_samples <- combinations$num_high <- combinations$diff <- combinations$pval <-  combinations$W <- NA
+    combinations$drug <- combinations$feature <- NA
     
     # initiate row count
     row = 1
+    print(paste("Number of features:", length(features)))
+
+    for (i in seq_along(features)) {
         
-    # binarize transcript expression by median
-    subset <- counts_df[complete.cases(counts_df),,drop=F]
+        print(i)
+        feature <- features[i]
 
-    high_exp <- rownames(subset)[as.vector(subset >= median)]
-    low_exp <- rownames(subset)[-which(rownames(subset) %in% high_exp)]
+        # binarize transcript expression by median (0)
+        subset <- counts_df[,i, drop = FALSE]
+        subset[subset == 0] <- NA
 
-    # loop through each drug
+        high_exp <- rownames(subset[complete.cases(subset), , drop = FALSE])
+        low_exp <- rownames(subset)[-which(rownames(subset) %in% high_exp)]
 
-    for (j in seq_along(rownames(drug_df))) {
-        
-        high = drug_df[j,colnames(drug_df) %in% high_exp] |> as.numeric()
-        low = drug_df[j,colnames(drug_df) %in% low_exp] |> as.numeric()
+        # loop through each drug
 
-        if (length(high[!is.na(high)]) > 1 & length(low[!is.na(low)]) > 1) {
+        for (j in seq_along(rownames(drug_df))) {
+            
+            high = drug_df[j,colnames(drug_df) %in% high_exp] |> as.numeric()
+            low = drug_df[j,colnames(drug_df) %in% low_exp] |> as.numeric()
 
-            # wilcoxon rank sum test
-            res <- wilcox.test(high, low, alternative = "two.sided", exact = FALSE)
+            if (length(high[!is.na(high)]) > 1 & length(low[!is.na(low)]) > 1) {
 
-            # save results
-            combinations$W[row] <- res$statistic
-            combinations$pval[row] <- res$p.value
-            combinations$num_samples[row] <- nrow(subset)
+                # wilcoxon rank sum test
+                res <- wilcox.test(high, low, alternative = "two.sided", exact = FALSE)
 
-        } else {
-            # save results
-            combinations$W[row] <- NA
-            combinations$pval[row] <- NA
-            combinations$num_samples[row] <- nrow(subset)
+                # difference in average AAC of two groups
+                AAC_diff <- mean(high[!is.na(high)]) - mean(low[!is.na(low)])
+
+                # save results
+                combinations$W[row] <- res$statistic
+                combinations$pval[row] <- res$p.value
+                combinations$diff[row] <- AAC_diff
+                combinations$num_samples[row] <- nrow(subset)
+                combinations$num_high[row] <- length(high_exp)
+                combinations$feature[row] <- feature
+                combinations$drug[row] <- rownames(drug_df)[j]
+
+            } else {
+                # save results
+                combinations$W[row] <- NA
+                combinations$pval[row] <- NA
+                combinations$diff[row] <- NA
+                combinations$num_samples[row] <- nrow(subset)
+                combinations$num_high[row] <- NA
+                combinations$feature[row] <- feature
+                combinations$drug[row] <- rownames(drug_df)[j]
+            }
+
+            row <- row + 1
         }
-
-        row <- row + 1
-        
     }
-    combinations$pset <- pset
     combinations$FDR <- p.adjust(combinations$pval, method = "BH")
 
     # format dataframe for plotting 
     combinations <- combinations[order(combinations$W, decreasing = T),]
     combinations$rank <- seq_len(nrow(combinations))
     combinations$pair <- paste(combinations$Drug, combinations$Feature, sep = "_")
+    combinations$label <- label
     return(combinations)
 }
 
-belinostat_res <- rbind(binary_dr(ciri_ccle_ge, belinostat, "CIRI2"),
-                        binary_dr(circ_ccle_ge, belinostat, "CIRCexplorer2"),
-                        binary_dr(cfnd_ccle_ge, belinostat, "circRNA_finder"),
-                        binary_dr(fcrc_ccle_ge, belinostat, "find_circ"))
+
+ciri_gcsi_bin <- binary_dr(ciri_gcsi, gcsi_sen, "CIRI_gCSI")
+ciri_gdsc_bin <- binary_dr(ciri_gdsc, gdsc_sen, "CIRI_GDSC")
+ciri_ccle_bin <- binary_dr(ciri_ccle, ccle_sen, "CIRI_CCLE")
+
+circ_gcsi_bin <- binary_dr(circ_gcsi, gcsi_sen, "CIRC_gCSI")
+circ_gdsc_bin <- binary_dr(circ_gdsc, gdsc_sen, "CIRC_GDSC")
+circ_ccle_bin <- binary_dr(circ_ccle, ccle_sen, "CIRC_CCLE")
+
+cfnd_gcsi_bin <- binary_dr(cfnd_gcsi, gcsi_sen, "CFND_gCSI")
+cfnd_gdsc_bin <- binary_dr(cfnd_gdsc, gdsc_sen, "CFND_GDSC")
+cfnd_ccle_bin <- binary_dr(cfnd_ccle, ccle_sen, "CFND_CCLE")
+
+fcrc_gcsi_bin <- binary_dr(fcrc_gcsi, gcsi_sen, "FCRC_gCSI")
+fcrc_gdsc_bin <- binary_dr(fcrc_gdsc, gdsc_sen, "FCRC_GDSC")
+fcrc_ccle_bin <- binary_dr(fcrc_ccle, ccle_sen, "FCRC_CCLE")
+
+# save dataframes
+save(ciri_gcsi_bin, ciri_gdsc_bin, ciri_ccle_bin,
+     circ_gcsi_bin, circ_gdsc_bin, circ_ccle_bin,
+     cfnd_gcsi_bin, cfnd_gdsc_bin, cfnd_ccle_bin,
+     fcrc_gcsi_bin, fcrc_gdsc_bin, fcrc_ccle_bin,
+     file = "../results/data/circ_myc_bindr.RData")
+
 
 
 ############################################################
-# Plot binary drug response association
+# Volcano Plots
 ############################################################
 
-# function to create dataframe for binary drug response plot
-binarize_myc <- function(counts_df, drug_df) {
+plot_volcano <- function(bin_dr) {
 
-    # compute median
-    median <- median(counts_df$MYC, na.rm = T)
-    
-    # binarize transcript expression by median
-    subset <- counts_df[complete.cases(counts_df),,drop=F]
-    high_exp <- rownames(subset)[as.vector(subset >= median)]
-    low_exp <- rownames(subset)[-which(rownames(subset) %in% high_exp)]
+    bin_dr <- bin_dr[!is.na(bin_dr$diff),]
 
-    # create dataframe
-    toPlot <- rbind(t(drug_df[j,colnames(drug_df) %in% high_exp]), t(drug_df[j,colnames(drug_df) %in% low_exp])) |> as.data.frame()
-    toPlot$label <- ifelse(rownames(toPlot) %in% high_exp, "high", "low")
-    rownames(toPlot) <- NULL
+    # label overlapping associations
+    bin_dr$temp <- paste(bin_dr$pair, bin_dr$label, sep = "_")
+    to_keep <- bin_dr[bin_dr$FDR < 0.05,]$temp
+    bin_dr$to_label <- ifelse(bin_dr$temp %in% to_keep, as.character(bin_dr$Drug), "")
 
-    return(toPlot)
+
+    # label by significance
+    bin_dr$Label <- ifelse(bin_dr$FDR < 0.05, "FDR\nSignificant", "Not FDR\nSignificant")
+    bin_dr$Label <- factor(bin_dr$Label, levels = c("FDR\nSignificant", "Not FDR\nSignificant"))
+
+
+    # get x limits
+    x <- max(c(abs(min(bin_dr$diff)), max(bin_dr$diff)))
+
+    # plot
+    p <- ggplot() +
+        geom_point(data = bin_dr, aes(x = diff, y = -log(FDR)), color = "gray") +
+        geom_point(data = bin_dr[bin_dr$Label == "FDR\nSignificant", ],
+                    aes(x = diff, y = -log(FDR), color = label)) +
+        geom_text_repel(data = bin_dr[bin_dr$Label == "FDR\nSignificant", ],
+                        aes(x = diff, y = -log(FDR), label = to_label),
+                        force_pull = 0.5, max.overlaps = Inf) +
+        scale_color_manual(
+            values = c("FCRC_GDSC" = "#B23A48", "FCRC_CCLE" = "#683ab2"), 
+            labels = c("FCRC_GDSC" = "GDSC2 (find_circ)", "FCRC_CCLE" = "CCLE (find_circ)"),
+            name = "PSet (Pipeline)") +
+        theme_classic() + xlim(-x, x) +
+        theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5),
+                legend.key.size = unit(0.5, 'cm')) +
+        labs(x = "Difference in Average AAC", y = "-log(FDR)")
+    return(p)
+
 }
 
-# function to plot box plot
-plot_box <- function(toPlot, drugname, filename) {
-    colnames(toPlot)[1] <- "Drug"
-    png(paste0(filename, ".png"), width = 4, height = 4, res = 600, units = "in")
-    print({
-        ggplot(toPlot, aes(x = label, y = Drug, fill = label)) + geom_boxplot() +
-            geom_jitter(shape=16, position=position_jitter(0.2)) +
-            scale_fill_manual("MYC Expression", values = c("#B23A48", "#3E92CC")) + theme_classic() +
-            labs(x = "MYC Expression", y = paste(drugname, "Response (AAC)"))
-    })
-    dev.off()
-}
+toPlot <- rbind(ciri_gcsi_bin, ciri_gdsc_bin, ciri_ccle_bin,
+                circ_gcsi_bin, circ_gdsc_bin, circ_ccle_bin,
+                cfnd_gcsi_bin, cfnd_gdsc_bin, cfnd_ccle_bin,
+                fcrc_gcsi_bin, fcrc_gdsc_bin, fcrc_ccle_bin)
 
-plot_box(binarize_myc(ciri_ccle_ge, belinostat), "Belinostat", "CIRI_CCLE_Belinostat")
-plot_box(binarize_myc(circ_ccle_ge, belinostat), "Belinostat", "CIRC_CCLE_Belinostat")
-plot_box(binarize_myc(cfnd_ccle_ge, belinostat), "Belinostat", "CFND_CCLE_Belinostat")
-plot_box(binarize_myc(fcrc_ccle_ge, belinostat), "Belinostat", "FCRC_CCLE_Belinostat")
-
-belinostat_plot <- rbind(binarize_myc(ciri_ccle_ge, belinostat),
-                         binarize_myc(circ_ccle_ge, belinostat),
-                         binarize_myc(cfnd_ccle_ge, belinostat),
-                         binarize_myc(fcrc_ccle_ge, belinostat))
-
-plot_box(belinostat_plot, "Belinostat", "CCLE_Belinostat")
-
+png("../results/figures/figure9/myc/volcano.png", width = 6, height = 4, res = 600, units = "in")
+plot_volcano(toPlot)
+dev.off()
